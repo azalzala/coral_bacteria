c676d28349c1f92615c71f4bbeec8aff5115ba7b
azalzala <153948468+azalzala@users.noreply.github.com>
2026-03-30 14:39:04 +0100

    Update sra.sh

--- sra.sh
+++ sra.sh
@@ -9,14 +9,14 @@ if [[ $# -lt 2 ]]; then
     exit 1
 fi
 
-PRJ="$1"          # e.g. PRJNA257197
-OUTDIR="$2"       # where FASTQs will go
-SRA_LIST="$3"
-JOBS="${JOBS:-4}" # parallel jobs for fasterq-dump
+PRJ="$1"          #PRJNA identifier
+OUTDIR="$2"       # output directory (choose PRJNA identifier or study number) 
+SRA_LIST="$3"   # accession list available in accessions
+JOBS="${JOBS:-4}" # parallel jobs for fasterq-dump 
 
 mkdir -p "${OUTDIR}"
 
-# 2) Loop over SRR accessions and download FASTQ using fasterq-dump
+# Loop over SRR accessions and download FASTQ using fasterq-dump
 while read -r SRR; do
    
    [[ -z "${SRR}" ]] && continue
